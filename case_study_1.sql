1911c945594ff9bfe293e5b84ce21d129ecf5ebc
Hashim Hashim <50488438+HashimH-5@users.noreply.github.com>
2023-08-29 23:12:33 +0300

    rename to case_study_1.sql

--- case_study_1.sql
+++ case_study_1.sql
@@ -1,6 +1,26 @@
+/* --------------------
+   Case Study Questions
+   --------------------*/
+---Tables
+--  sales
+--  members
+--  menu
+
+-- 1. What is the total amount each customer spent at the restaurant?
+-- 2. How many days has each customer visited the restaurant?
+-- 3. What was the first item from the menu purchased by each customer?
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+-- 5. Which item was the most popular for each customer?
+-- 6. Which item was purchased first by the customer after they became a member?
+-- 7. Which item was purchased just before the customer became a member?
+-- 8. What is the total items and amount spent for each member before they became a member?
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
 -- 10. In the first week after a customer joins the program (including their join date) they earn
 2x points on all items, not just sushi - how many points do customer A and B have at the end of
 January?
+ 
 WITH members AS (
 
 SELECT s.customer_id, s.product_id, order_date, join_date
@@ -23,6 +43,7 @@ GROUP BY 1
 
 -- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many
 points would each customer have?
+ 
 WITH first_orders AS (
 SELECT s.customer_id, m.product_name,price, CASE WHEN m.product_name= 'sushi' THEN
 m.price*20 ELSE m.price*10 END AS points
